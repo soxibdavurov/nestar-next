2ecaa078cde347061ce91313effe530d09cc58dc
soxibdavurov <soxib.davurov@gmail.com>
2025-10-21 11:31:54 +0900

    fix: modify GraphQL query command

--- apollo/admin/query.ts
+++ apollo/admin/query.ts
@@ -4,6 +4,49 @@ import { gql } from '@apollo/client';
  *         MEMBER         *
  *************************/
 
+export const GET_MEMBER = gql(`
+	query GetMember($input: String!) {
+    getMember(memberId: $input) {
+        _id
+        memberType
+        memberStatus
+        memberAuthType
+        memberPhone
+        memberNick
+        memberFullName
+        memberImage
+        memberAddress
+        memberDesc
+        memberProperties
+        memberArticles
+        memberFollowers
+        memberFollowings
+        memberPoints
+        memberLikes
+        memberViews
+        memberComments
+        memberRank
+        memberWarnings
+        memberBlocks
+        deletedAt
+        createdAt
+        updatedAt
+        accessToken
+        meLiked {
+            memberId
+            likeRefId
+            myFavorite
+        }
+        meFollowed {
+            followingId
+            followerId
+            myFollowing
+        }
+    }
+}
+	`);
+
+
 export const GET_ALL_MEMBERS_BY_ADMIN = gql`
 	query GetAllMembersByAdmin($input: MembersInquiry!) {
 		getAllMembersByAdmin(input: $input) {
