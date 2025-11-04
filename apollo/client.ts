bcfb9fbb05d610579b4d797bc3cfd21fce4cd60c
soxibdavurov <soxib.davurov@gmail.com>
2025-11-04 09:50:52 +0900

    feat: develop WebSocket Chatting Client first stage

--- apollo/client.ts
+++ apollo/client.ts
@@ -28,6 +28,36 @@ const tokenRefreshLink = new TokenRefreshLink({
 	},
 });
 
+// Custom WebSocket client
+class LoggingWebSocket {
+	private socket: WebSocket;
+
+	constructor(url: string) {
+		this.socket = new WebSocket(url);
+
+		this.socket.onopen = () => {
+			console.log('WebSocket connection!');
+		};
+
+		this.socket.onmessage = (msg) => {
+			console.log('WebSocket message:', msg.data);
+		};
+
+		this.socket.onerror = (error) => {
+			console.log('WebSocket error:', error);
+		};
+	}
+
+	send(data: string | ArrayBuffer | SharedArrayBuffer | Blob | ArrayBufferView) {
+		this.socket.send(data);
+	}
+
+	close() {
+		this.socket.close();
+	}
+}
+
+
 function createIsomorphicLink() {
 	if (typeof window !== 'undefined') {
 		const authLink = new ApolloLink((operation, forward) => {
@@ -56,6 +86,7 @@ function createIsomorphicLink() {
 					return { headers: getHeaders() };
 				},
 			},
+			webSocketImpl: LoggingWebSocket,
 		});
 		console.log('GRAPHQL URL:', process.env.REACT_APP_API_GRAPHQL_URL);
 		const errorLink = onError(({ graphQLErrors, networkError, response }) => {
